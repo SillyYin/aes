7bc495b910bc18eb2f67c25c7cee9dd8a2ebc76f
Yin <Yin@YinMacBook-Pro.local>
2017-06-04 03:49:58 +0800

    first

---
+++ cmake-build-debug/CMakeFiles/aes_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aes_1.dir/main.cpp.o"
+  "aes_1.pdb"
+  "aes_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aes_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
